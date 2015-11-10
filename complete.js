e7e3775a7bb2ea9a9b504728b9fde43d927c0ab7
yashasm <yashasm1991@gmail.com>
2015-11-10 00:11:19 -0800

    Initial commit

--- complete.js
+++ complete.js
@@ -1,18 +1,16 @@
 window.addEventListener('load', function(){
-//var x = 20;
-//var y = 10;
-//var w = 20;
-//var h = 10;
-
-//var speed = 2;
-//var ySpeed = 1;
 var level = 1;
 var canvas = document.getElementById("myCanvas");
 var ctx =canvas.getContext("2d");
 var GAME_WIDTH = 640;
 var GAME_HEIGHT = 360;
 var gameAlive = true;
-    var sprites= {};
+var sprites= {};
+
+//array of enemy location.
+//x and y represents location of the enemy blocks.
+//w and h represents width and height of enemy blocks.
+//speedY represents, spped at which enemy blocks move.
 var enemies = [
     {x:100,
     y: 100,
@@ -36,6 +34,7 @@ var enemies = [
     h: 30}
 ];
 
+//player location and details.
 var player = {x:10,
     y: 160,
     speedX: 2,
@@ -43,40 +42,41 @@ var player = {x:10,
     h: 30,
     isMoving: false         
     };
-
+//details of goal location
 var goal = {x:550   ,
     y: 160,    
     w: 30,
     h: 30,                
     };
 
+//called during mouse-click event or touch event to move the player in x axis.
 var movePlayer = function(){
     player.isMoving = true;
 };
-
+//called during mouse-click event or touch event to stop the player
 var stopPlayer = function(){
     player.isMoving = false;
 };
 
-    var reload = function(){
-        player.x = 10;
-        player.y = 160;     
-        player.speedX = player.speedX +1;
-        player.isMoving = false;
-    };
+//reload function resets player location after the game.
+var reload = function(){
+    player.x = 10;
+    player.y = 160;     
+    player.speedX = player.speedX +1;
+    player.isMoving = false;
+};
     
 canvas.addEventListener('mousedown',movePlayer);
 canvas.addEventListener('mouseup',stopPlayer);
 canvas.addEventListener('touchstart',movePlayer);
 canvas.addEventListener('touchend',stopPlayer);    
     
+//update function, updates x and y axis of player and enemy location.	
 var update = function(){
     
-if(checkCollision(player,goal)){
-    //gameAlive = false;              
+if(checkCollision(player,goal)){    
     alert('Next Level !!');
-    //window.location = "";                
-    
+                  
     enemies.forEach(function(element,index){
         if (element.speedY > 0){
         element.speedY += 1;
@@ -92,32 +92,33 @@ if(checkCollision(player,goal)){
     }
     level += 1;
     reload();
-}
-        if(player.isMoving){
-            player.x += player.speedX;
+	}
+    if(player.isMoving){
+        player.x += player.speedX;
+    }
+    enemies.forEach(function(element,index){
+    
+    if(checkCollision(player,element)){
+        gameAlive = false;
+        alert('Game Over!!');
+        window.location = "";
+    }
+            
+    element.y += element.speedY;
+    
+    if (element.y <= 10){
+        element.y = 10;
+        element.speedY *= -1;
         }
-        enemies.forEach(function(element,index){
-        
-        if(checkCollision(player,element)){
-            gameAlive = false;
-            alert('Game Over!!');
-            window.location = "";
-        }
-                
-        element.y += element.speedY;
-        
-        if (element.y <= 10){
-            element.y = 10;
-            element.speedY *= -1;
-            }
-        else if(element.y >= GAME_HEIGHT-50){
-            element.y = GAME_HEIGHT - 60;
-            element.speedY *= -1;
-        }
+    else if(element.y >= GAME_HEIGHT-50){
+        element.y = GAME_HEIGHT - 60;
+        element.speedY *= -1;
+    }
     });
     
 };
 
+//load function loads the image at the start of the game.
 var load = function(){
     sprites.player = new Image();
     sprites.player.src = "images/hero.png";
@@ -132,6 +133,7 @@ var load = function(){
     sprites.goal.src = "images/chest.png";
 };
 
+//draw function redraws the player enemy location after each frame.
 var draw = function(){
     ctx.drawImage(sprites.background,0,0);    
     ctx.drawImage(sprites.player,player.x,player.y);    
@@ -143,6 +145,7 @@ var draw = function(){
 
 };
 
+//checkCollision function provides collision detection unit.
 var checkCollision = function(rect1,rect2){
     var closeOnWidth = Math.abs(rect1.x-rect2.x) <= Math.max(rect1.w,rect2.w);
     var closeOnHeight = Math.abs(rect1.y-rect2.y) <= Math.max(rect1.h,rect2.h);    
